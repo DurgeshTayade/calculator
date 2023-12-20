de89f6984eeb3c79329a8141a20ed76bda2d73e7
DurgeshTayade <durgeshtayade1145@gmail.com>
2023-12-20 09:45:49 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Navbar from './components/Navbar';
+import Calc from './components/Calc';
+import Footer from './components/Footer';
+function App() {
+  return (
+   <div>
+      <Navbar />
+      <Calc />
+      <Footer />
+   </div>
+  );
+}
+
+export default App;
